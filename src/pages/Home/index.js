45230d86ec929ef89081f615acc7ba1dd5df375b
robertvitoriano <robertvitorianodev@gmail.com>
2020-09-17 15:34:53 -0300

    improving paragraph apearance and adding new photo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,38 +1,36 @@
 import React from "react";
 import Header from "./../../components/Header";
 import Footer from './../../components/Footer'
-import profilePic from "./../../assets/img/profile-picture.jpg";
+import profilePic from "./../../assets/img/Mask Group 1.png";
 const Home = ({match,history}) => {
   return (
-    <div class="home-container container">
+    <div>
       <Header match={match} history={history}></Header>
-      <section class="profile">
-        <h1>Robert da Silva Vitoriano</h1>
+      <section class="home-container container">
+        <h1>Olá!<br/> me chamo Robert. Seja bem-vindo(a) ao meu portifólio.</h1>
         <div class="information">
           <div class="photo-wrapper">
             <img src={profilePic} style={{ height: "200px" }} alt="" />
           </div>
           <div class="about">
             <p>
-              Cursando o 3º Semestre de Análise e Desenvolvimento de Sistemas na
+              Cursando o 4º Semestre de Análise e Desenvolvimento de Sistemas na
               fatec Sorocaba. Embora esteja a relativamento pouco tempo me
               aventurando na área de T.I. Desenvolvi muitos interesses no ramo
-              ao longo desses 3 semestre. Sendo dentre estes os principais, aos
-              quais foco meus estudos:
+              ao longo desses 4 semestres. Meus <strong>estudos atuais</strong> se concentram  em :
             </p>
-            <div class="skills-list">
+            <div class="skills-list-container">
               <ul class="skills">
                 <li>Fullstack(Node.Js e React)</li>
-                <li>Desenvolvimento de aplicativos(React Native)</li>
+                <li>Desenvolvimento mobile(React Native)</li>
                 <li>Programação de Jogos(Unity)</li>
-                <li>Linux</li>
               </ul>
             </div>
             <p>
               Destas áreas citadas, a que mais me despertou paixão pelo estudo
               foi o <strong>desenvolvimento fullstack</strong>. Ver todos os
               conhecimentos de um determinado conjunto de tecnologias se
-              encaixando para criar uma aplicação Web é uma experiência
+              encaixando para criar uma <strong>aplicação Web</strong> é uma experiência
               incrível. Tão incrível que todas as vezes que me dedico a criar
               algo, sinto a mesma euforia que senti quando fiz meu primeiro
               server local.
