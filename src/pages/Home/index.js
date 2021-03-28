2106c5692cc6a9d8b7581def778f07d740364dd8
robertvitoriano <robertvitorianodev@gmail.com>
2021-03-27 21:35:53 -0300

    update data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -19,7 +19,7 @@ const Home = ({ match, history }) => {
         <div class="information">
           <div class="about">
             <p>
-              Tenho 22 e atualmente estou Cursando o 4º Semestre de Análise e
+              Tenho 23 e atualmente estou Cursando o 5º Semestre de Análise e
               Desenvolvimento de Sistemas na Fatec Sorocaba.
             </p>
 
