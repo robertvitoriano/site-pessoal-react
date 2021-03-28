258189ab52196baa77c5eb3d925e520d9eb3bf0c
robertvitoriano <robertvitorianodev@gmail.com>
2021-03-27 21:35:12 -0300

    remove portifolio page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from 'react'
 import Home from './pages/Home';
-import Portifolio from './pages/Portifolio'
 import Header from './components/Header'
 
 export default function Routes() {
@@ -10,7 +9,6 @@ export default function Routes() {
       <Header/>
       <Switch>
         <Route path="/about" exact component={Home} />
-        <Route path="/portifolio" component={Portifolio} />
       </Switch>
     </Router>)
 
