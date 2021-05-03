3f15880461c297d06806f9abac95afd93b1b9f76
Roberty Vitoriano <robertvitoriano@gmail.com>
2021-05-02 22:51:25 -0300

    make navbar responsive

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import logo from "./../../assets/img/Logo-corel-variação-2-optmized.svg";
+import Hamburger_icon from './../../assets/img/Hamburger_icon.svg'
 function Header({ history }) {
   function handlePortifolio() {
     history.push("/Portifolio");
   }
-  return (
-    <div class="navbar">
+
+
+  const [showSideBarMenu, setShowSideBarMenu] = useState(false)
+  const [leftSideBarPosition, setLeftSideBarPosition] = useState(false)
+  const [sideBarClass, setsideBarClass] = useState("side-bar-hide")
+
+  useEffect(()=>{
+        
+    setsideBarClass(showSideBarMenu?'side-bar-show': 'side-bar-hide')
+
+  },[showSideBarMenu])
+
+  
+  return (<>
+  <img src={Hamburger_icon} class="hamburger-icon"  onClick ={e => setShowSideBarMenu(!showSideBarMenu)}/>
+    <div class={`navbar ${sideBarClass}`}>
       <Link to="/about">
         <img className="logo" alt="portifolio logo" src={logo}></img>
       </Link>
@@ -26,11 +41,15 @@ function Header({ history }) {
         <li class="menu-item">
           <Link className="header-link" to="/contact">Contato</Link>
         </li>
+        <li className="blog-link-container-mobile">
+        <Link to="/blog">Blog</Link>
+      </li>
       </ul>
-      <div className="blog-link-container">
+      <div className="blog-link-container-desktop">
         <Link to="/blog">Blog</Link>
       </div>
     </div>
+    </>
   );
 }
 
